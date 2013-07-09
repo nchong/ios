5cef77302791e9028bb3d6b1da2dd4c9ae535499
Nathan <nathan.chong@gmail.com>
2013-07-09 05:32:34 +0100

    Cleanup makefile

--- src/abstraction.h
+++ src/abstraction.h
@@ -20,9 +20,9 @@ DECLARE_UF_BINARY(CIRC, int, int, int);
 #define TYPE unsigned
 #define OPERATOR(X, Y) ((X == IDENTITY) ? Y : ((Y ==  IDENTITY) ? X : ((X == TOP) | (Y == TOP) | (GET_UPPER(X) != GET_LOWER(Y)) ? TOP : MAKE_PAIR(GET_LOWER(X), GET_UPPER(Y)))))
 #else
-#define MAKE_PAIR(lower, upper) (0x0000000000000000 | (((lower) << 16) | (upper)))
-#define GET_UPPER(X) ((X) & (0x000000000000ffff))
-#define GET_LOWER(X) (((X) >> 16) & (0x000000000000ffff))
+#define MAKE_PAIR(lower, upper) (((lower) << 16) | (upper))
+#define GET_UPPER(X) ((X) & (0x0000ffff))
+#define GET_LOWER(X) (((X) >> 16) & (0x0000ffff))
 
 // Identity is identified by bad interval (1, 0)
 #define IDENTITY MAKE_PAIR(1, 0)
